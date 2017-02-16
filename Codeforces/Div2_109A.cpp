b7ac35cadb924ebab7e972c9f579cd181fb3006c
Pabfigueira <pablo.luiz10@gmail.com>
2017-02-15 22:08:11 -0200

    new

---
+++ Codeforces/Div2_109A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define imprime(x, Y)                              \
+  for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+  cerr << endl;
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+int main() {
+
+    int n, i, mini, maxi, num, cont; 
+    
+    sc(n);
+
+    sc(num);
+
+    mini = num;
+    maxi = num;
+    cont = 0;
+
+    for( i=1; i<n; i++ ) {
+        cin >> num;    
+        if( num < mini ) {
+            mini = num;
+            cont++;
+            if( num > maxi ) maxi = num; 
+        } else if( num > maxi ) { 
+            maxi = num;
+            cont++;
+        }
+    }
+    pri(cont);
+    return 0;
+}
