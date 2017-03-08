ac1ae0895705c070c7e579cc01ebb2a644c98169
Pabfigueira <pablo.luiz10@gmail.com>
2017-03-08 14:02:05 -0300

    new

---
+++ UVA/UVA195.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define imprime(x, Y)                              \
+		for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+cerr << endl;
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+bool myComp( const char& l, const char& r ) {
+    if( tolower(l) == tolower(r) )
+        return l < r;
+    else
+        return tolower(l) < tolower(r);
+}
+
+int main() {
+
+    int n, i;
+    string word;
+    sc(n);
+    for( i=0; i<n; i++ ) {
+        cin >> word;        
+
+        sort(word.begin(), word.end(), myComp);
+
+        do {
+            cout << word << endl;
+        } while( next_permutation( word.begin(),word.end(), myComp ) );
+    }
+	
+	return 0;
+}
