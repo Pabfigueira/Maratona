9ba9471b219b25ce67c6c5f8e6f5022018ff7cee
Pabfigueira <pablo.luiz10@gmail.com>
2017-03-28 09:34:23 -0300

    new

---
+++ Timus/1225.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define imprime(x, Y)                              \
+  for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+  cerr << endl;
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+int main() {
+
+    int n, i;
+    
+    sc(n);
+
+    vector< vector<long long > > dp(n+1,vector<long long >(4,0));
+        
+    dp[1][1] = 1;
+    dp[1][2] = 0;
+    dp[1][3] = 1;
+
+    for( i=2; i<=n; i++ ) {
+        dp[i][1] = dp[i-1][3] + dp[i-2][3];
+        dp[i][3] = dp[i-1][1] + dp[i-2][1];
+    }  
+    cout << dp[n][1] + dp[n][3] << endl;
+    return 0;
+}
