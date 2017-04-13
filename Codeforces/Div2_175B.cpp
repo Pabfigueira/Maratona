90743b66a645e8379f38c288bc141d09a2bd4bf9
Pabfigueira <pablo.luiz10@gmail.com>
2017-04-13 15:51:48 -0300

    new

---
+++ Codeforces/Div2_175B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define imprime(x, Y)                              \
+        for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+cerr << endl;
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+int main() {
+    int n, i, s, t, passos;
+
+    sc3(n,s,t);
+    
+    vector<int> vetor(n+1);    
+    set<int> visited;
+
+    for( i=1; i<=n; i++ ){
+        sc(vetor[i]);
+    }
+
+    passos = 0;
+
+    while( s!=t ) {
+        visited.insert(s);
+        if( visited.find(vetor[s])!=visited.end() ) {
+            cout << -1 << endl;
+            return 0;
+        }
+        s = vetor[s];
+        passos++;
+    }
+    cout << passos << endl;
+    return 0;
+}
