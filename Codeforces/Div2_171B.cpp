c85bdd4689f3c0ae351bb127822fa4860342d2c0
Pabfigueira <pablo.luiz10@gmail.com>
2017-04-16 16:20:56 -0300

    new

---
+++ Codeforces/Div2_171B.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define imprime(x, Y)                              \
+        for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+cerr << endl;
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+int main() {
+    ll n, m, i, j, soma, maximo;
+
+    scanf("%lld %lld",&n, &m);
+
+    vector<ll> vetor(n);
+    
+    for( i=0; i<n; i++ ) {
+        scanf("%lld",&vetor[i]);
+    } 
+
+    i=0; 
+    j=0;
+    soma = 0;
+    maximo = 0;
+    while( j<n ) {
+        while( j<n && soma<=m ) {
+            soma += vetor[j];
+            if( soma<=m ) 
+                j++;
+        } 
+        maximo = max(maximo,j-i);
+        while( soma>m ) {
+            soma -= vetor[i];
+            i++;
+        }
+        j++;   
+    }
+    printf("%lld\n",maximo);
+    return 0;
+}
