c3e27f5cfb9d550366c5595cd34fa4673b52d191
Pabfigueira <pablo.luiz10@gmail.com>
2017-08-01 16:41:13 -0300

    adhoc

---
+++ Codeforces/Div2_CodeforcesBetaRound76_A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define imprime(x, Y)                              \
+        for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+cerr << endl;
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+int main() {
+    int i, j;
+    map<string,int> mapa;
+    vector< string > pass(8);
+    char letter;
+    string word;
+
+    for( i=0; i<8; i++ ) {
+        for( j=0; j<10; j++ ) {
+            cin >> letter;
+            pass[i] += letter;    
+        }
+    }
+    
+    for( i=0; i<10; i++ ) {
+        cin >> word;
+        mapa[word] = i;
+    }
+
+    for( i=0; i<8; i++ ) {
+        cout << mapa[pass[i]];
+    }
+    cout << endl;
+
+    return 0;
+}
