12a3341e73ce76575c6975d1fe9cbf40e63fdd6f
Pabfigueira <pablo.luiz10@gmail.com>
2017-08-01 00:04:59 -0300

    adhoc

---
+++ Codeforces/IX_Samara_Regional_IntercollegiateProgrammingContest_B.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define imprime(x, Y)                              \
+        for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+cerr << endl;
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+int main() {
+    int n, i;
+
+    sc(n);
+
+    list<int> error;
+    vector<int> vetor(n);
+
+    for( i=0; i<n; i++ ) {
+        sc(vetor[i]);
+        if( vetor[i] == i+1 ) error.pb(i);
+    }
+    printf("%d\n",(error.size()+1)/2);
+    while( !error.empty() ) {
+        if( error.size() == 1 ) {
+            printf("%d ", error.front()+1);
+            if( error.front()!=0 ) printf("%d\n",1);
+            else printf("%d\n",2);
+            break;
+        }
+        printf("%d ", error.front()+1 );
+        error.pop_front();
+        printf("%d\n", error.front()+1 );
+        error.pop_front();      
+    }
+    return 0;
+}
