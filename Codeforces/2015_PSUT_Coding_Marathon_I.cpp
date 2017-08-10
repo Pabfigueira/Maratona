0491fbe736f52dff0a8d0f4b5176536f8bdc6cc1
Pabfigueira <pablo.luiz10@gmail.com>
2017-08-10 19:43:17 -0300

    adhoc

---
+++ Codeforces/2015_PSUT_Coding_Marathon_I.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a,&b)
+#define sc3(a, b, c) scanf("%d%d%d",&a, &b,&c)
+#define scs( a ) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define f first
+#define s second
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+
+const int inf = 0x3f3f3f3f;
+const ll linf = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9+7;
+
+int main() {
+
+    ll n, h, m, s, hh, mm, ss, one, two, i;
+    char pt;
+    bool error = false;
+
+    cin >> n;
+
+    for( i=0; i<n; i++ ) {
+        cin >> h >> pt >> m >> pt >> s >> hh >> pt >> mm >> pt >> ss;
+        one = (h*60*60) + (m*60) + s;
+        two = (hh*60*60) + (mm*60) + ss;
+        if( two-one >= 3*60*60 ) error = true; 
+    }   
+    if( error ) cout << "No\n";
+    else cout << "Yes\n";
+    return 0;
+}
