e2704361a28733f7a1d79c8ac4dc6ce615646676
Pabfigueira <pablo.luiz10@gmail.com>
2018-02-22 17:18:38 -0300

    adhoc

---
+++ Codeforces/Div2_111A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define sc3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define scs(a) scanf("%s", a)
+#define pri(x) printf("%d\n", x)
+#define prie(x) printf("%d ", x)
+#define mp make_pair
+#define pb push_back
+#define BUFF ios::sync_with_stdio(false);
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define imprime(x, Y)                              \
+		for (int X = 0; X < Y; X++) cerr << x[X] << " "; \
+cerr << endl;
+#define ler freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ld pi = acos(-1);
+const int MOD = 1e9 + 7;
+
+int main() {
+    ll n, i, soma, qtd, rest;
+
+    scanf("%lld",&n);
+
+    vector<ll> vetor(n);
+
+    soma = 0;
+    qtd = 0;
+    rest = 0;
+
+    for( i=0; i<n; i++ ) {
+        scanf("%lld",&vetor[i]);
+        soma += vetor[i];
+    }	
+    
+    sort(vetor.begin(),vetor.end());
+
+    for( i=n-1; i>=0; i-- ) {
+        rest += vetor[i];
+        soma -= vetor[i];
+        qtd++;
+        if( rest > soma ) break;        
+    }
+    printf("%lld\n",qtd);
+	return 0;
+}
